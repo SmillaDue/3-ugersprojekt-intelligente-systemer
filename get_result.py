cfaf5f2943d13a97cc2b1f1d0d3cd96ca039d4fc
SmillaDue <74299185+SmillaDue@users.noreply.github.com>
2021-01-08 18:30:48 +0100

    Update get_result.py

--- get_result.py
+++ get_result.py
@@ -7,23 +7,42 @@ Created on Thu Jan  7 17:58:29 2021
 """
 
 import numpy as np
+from correct_data import correct_data
+from correct_dataset import correct_dataset 
+from bath_processing import batch_processing
+import pandas as pd 
 
-def get_result(data,dataset):
-    #transforms data(the one recieved from bath_processing function) into ordered numpy
-    data=np.array(sorted(data, key=lambda tup: tup[0]))
-    value=np.zeros(len(data))
+def get_result(img_path, file):
+    data=batch_processing(img_path)
+    #corrects the data(the one recieved from bath_processing function) 
+    datasetCorrect=correct_dataset(data, file)
+    dataCorrect=correct_data(data, datasetCorrect)
+    #sorts the data
+    dataSorted=np.array(sorted(dataCorrect, key=lambda tup: tup[0]))
+    datasetSorted=np.array(sorted(datasetCorrect, key=lambda tup: tup[0]))
+    #print(dataSorted)
+    #print(len(dataSorted))
+    #print(datasetSorted)
+    #print(len(datasetSorted))
+    value=np.zeros(len(dataSorted))
     #checks if eyecolor in data is the same as eyecolor in dataset
     
-    for i in range(len(data)):
-        if data[i,1]==dataset[i,1]:
+    
+    for i in range(len(dataSorted)):
+        if dataSorted[i,1]=="Bad lighting":
+            value[i]=2
+        elif dataSorted[i,1]==datasetSorted[i,1]:
             value[i]=1
             
     #combines picturename and either 1 for same result or 0 for different result
-    result=np.array((data[:,0],value)).T
+    colors=np.array((dataSorted[:,1],datasetSorted[:,1])).T
+    points=np.array((dataSorted[:,0],value)).T
+    result=np.hstack((points,colors))
     #counts numbers of same result
-    correct=len(np.where(result=="1.0")[0])
-    length=len(data)
+    correct=len(np.where(points=='1.0')[0])
+    badLighting=len(np.where(points=='2.0')[0])
+    length=len(dataSorted)
+    pd.DataFrame(result).to_csv(img_path+'.csv')
     
-    return result, correct, length
-        
+    return result, correct, badLighting, length
         
