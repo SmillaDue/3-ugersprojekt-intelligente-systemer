f0d734479ac2714cd8066c4e975c2b3c4809a765
Rune-Nedergaard <76943893+Rune-Nedergaard@users.noreply.github.com>
2021-01-11 12:52:58 +0100

    Update extract_eyes.py

--- extract_eyes.py
+++ extract_eyes.py
@@ -10,7 +10,7 @@ import numpy as np
 import cv2
 from shape_to_np import shape_to_np
 import os
-from find_iris_data import find_iris_data
+from find_iris_data_extract import find_iris_data
 import glob
 
 
@@ -27,24 +27,34 @@ def extract_eyes(img_path,out_path):
         
         filename = os.path.basename(f)
         #udføre beskæring
-        iris = find_iris_data(filename)
-        
-        img = cv2.cvtColor(iris, cv2.COLOR_BGR2RGB)
-
-
-        width = int(50)
-        height = int(img.shape[0] / img.shape[1] * width)
-        dim = (width, height)
-        # resize image
-        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-               
-        #gemme beskåret del til ny mappe, med samme navn
-        
-        
-        cv2.imwrite(out_path+'/'+filename,resized)
-        
+        try:
+            iris = find_iris_data(img_path+'\\'+filename)
+            
+            #img = cv2.cvtColor(iris, cv2.COLOR_BGR2RGB)
+            img = iris
     
+    
+            width = int(50)
+            height = int(img.shape[0] / img.shape[1] * width)
+            dim = (width, height)
+            # resize image
+            resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+                   
+            #gemme beskåret del til ny mappe, med samme navn
+            
+            
+            cv2.imwrite(out_path+'/'+filename,resized)
+        except IndexError:
+            pass
         
+
+
+def all_colors(img_path, out_path):
+    extract_eyes(img_path+'/Brown',out_path+'/Brown')
+    extract_eyes(img_path+'/Green',out_path+'/Green')
+    extract_eyes(img_path+'/Blue',out_path+'/Blue')
+    extract_eyes(img_path+'/Hazel',out_path+'/Hazel')
+    
         
         
         
